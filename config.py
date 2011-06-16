692451e5aa4bee32561fc2334cc0e1243b4b2ea9
Gunnar Ro <gunnarroe@gmail.com>
2011-06-16 15:19:50 +0200

    Added gitignore

--- config.py
+++
@@ -1,6 +0,0 @@
-webroot='url_to_website'
-
-import os
-path=os.path.realpath(os.path.dirname(__file__))
-
-output_path=path+"/output"
--- dbConfig.py
+++
@@ -1,4 +0,0 @@
-host="db_host"
-login='db_username'
-password="db_password"
-database='db_name'
